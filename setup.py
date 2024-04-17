e38a4574a2d2d0bf961c810443d759a4417214a1
Susara Ouchithya <susara.ouchithya@gmail.com>
2024-04-17 23:37:26 +0530

    update 2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multidst',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),  # Automatically find all packages and subpackages
     install_requires=[
         'numpy', 
