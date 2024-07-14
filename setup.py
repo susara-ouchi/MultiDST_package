13fb4e587c3c3d6f59f94d494b22a5c1aa23ce89
Susara Ouchithya <susara.ouchithya@gmail.com>
2024-07-14 23:28:29 +0530

    publish-final-before-next-launch

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='multidst',
     version='0.1.3',
@@ -15,5 +20,7 @@ setup(
         "dev":["twine>4.0.2"],
     },
     python_requires=">=3.10",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
 
